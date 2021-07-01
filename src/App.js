33bb1b21402f4ed078bb2e9b9ed7af9846432596
afrin <afrin@wisdomcircle.in>
2021-07-01 17:33:42 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import LocationDetails from './components/LocationDetails';
+import LocationListing from './components/LocationListing';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Router>
+        <Switch>
+          <Route exact path={"/"} component={LocationListing} />
+          <Route exact path={"/location_details"} component={LocationDetails} />
+        </Switch>
+      </Router>
     </div>
+    
   );
 }
 
